e5beabf27c8987f0bad9a4fcf052f6dde463cca0
Álvaro Peralta <alvaroperalta@MacBook-Air-de-Alvaro.local>
2018-03-21 23:30:29 +0100

    Adding html reports

--- src/test/java/com/cucumberDemo/runner/Runner.java
+++ src/test/java/com/cucumberDemo/runner/Runner.java
@@ -4,10 +4,12 @@ import org.junit.runner.RunWith;
 
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber; 
 
-@RunWith(Cucumber.class) 
-@CucumberOptions(features = "src/test/resources", tags = "@login", glue="com.cucumberDemo.steps")
+
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources", tags = "@login", glue="com.cucumberDemo.steps", format = {"pretty", "html:target/Destination"}  )
 
 public class Runner {
 
